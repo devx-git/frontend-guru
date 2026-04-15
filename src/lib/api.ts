b281f21e00f9c9e260cd14d0c35f46f0de388dbe
adrian <devxsolutionsit@gmail.com>
2026-04-15 18:36:57 -0500

    correcciones1.1

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: "https://api.devxsolutions.pro",
   headers: {
     "Content-Type": "application/json",
   },
